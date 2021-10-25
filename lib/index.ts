25dcfd818316725a76e250ac64b3916cd4a012c1
Dylan J. Sather <dylan.sather@gmail.com>
2021-10-24 19:07:53 -0700

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import * as cdk from '@aws-cdk/core';
+
+export interface StatusPageDeadMansSwitchProps {
+  // Define construct properties here
+}
+
+export class StatusPageDeadMansSwitch extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: StatusPageDeadMansSwitchProps = {}) {
+    super(scope, id);
+
+    // Define construct contents here
+  }
+}
